c0ba0ce188ab73ded6545e3f837caef6d9f9943b
rpe4a <zapman2la@gmail.com>
2016-08-08 20:25:16 +0500

    delete logger, go redux devTools

--- src/js/store/configureStore.js
+++ src/js/store/configureStore.js
@@ -5,14 +5,21 @@ import rootReducer from '../reducers'
 export default (initialState) => {
     /*const logger = createLogger()*/
 
-    const store = createStore(
+
+    const finalCreateStore = compose(
+        applyMiddleware( /*logger*/ ),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )(createStore);
+
+    const store = finalCreateStore(
         rootReducer, //root reducer
         initialState, //default state
-        compose(
-            applyMiddleware( /*logger*/ ),
-            window.devToolsExtension ? window.devToolsExtension() : f => f
-        )
     );
+
+    /*const store = createStore(rootReducer, initialState, compose(
+        applyMiddleware(logger),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    ));*/
 
     if (module.hot) {
         module.hot.accept('../reducers', () => {
