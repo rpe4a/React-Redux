5b33866d3eb5b2d8bddd6dc03dc1dc94610807e7
Михаил Ю. Плюснин <plyusnin@iac.local>
2016-08-08 10:55:14 +0500

    start_project

---
+++ src/js/components/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className='container-fluid'>
+                Привет из App.js
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('bootstrap/dist/css/bootstrap.css');
+require('bootstrap/dist/js/bootstrap.js');
+require('css/_site.scss');
+
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+/*import { createStore } from 'redux'
+import { Provider } from 'react-redux'*/
+import App from './components/app'
+
+const 
+
+render(
+    <App />, document.getElementById('app')
+)
