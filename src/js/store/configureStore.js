a7d2f202bd5b250a7f2fac6500d6dec60b9685f9
rpe4a <zapman2la@gmail.com>
2016-08-08 19:50:10 +0500

    try setup logger

--- src/js/store/configureStore.js
+++ src/js/store/configureStore.js
@@ -1,22 +1,25 @@
-import { createStore, applyMiddleware  } from 'redux'
-import createLogger from 'redux-logger'
+import { createStore, applyMiddleware, compose } from 'redux'
+/*import createLogger from 'redux-logger'*/
 import rootReducer from '../reducers'
 
 export default (initialState) => {
-    const logger = createLogger()
+    /*const logger = createLogger()*/
 
     const store = createStore(
         rootReducer, //root reducer
         initialState, //default state
-        applyMiddleware(logger)
-    )
+        compose(
+            applyMiddleware( /*logger*/ ),
+            window.devToolsExtension ? window.devToolsExtension() : f => f
+        )
+    );
 
-    /*if (module.hot) {
+    if (module.hot) {
         module.hot.accept('../reducers', () => {
             const nextRootReducer = require('../reducers').default
             store.replaceReducer(nextRootReducer)
         })
-    }*/
- 
+    }
+
     return store;
 };
